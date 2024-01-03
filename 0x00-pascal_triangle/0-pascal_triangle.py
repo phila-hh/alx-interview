0d8c7ebbae57fa4fbf9b4f41aee86b404adc1fde
phila-hh <filimon.haftomh@gmail.com>
2024-01-03 03:47:39 -0800

    0x00. Pascal's Triangle project

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+def pascal_triangle(n):
+    pasTriList = []
+    m = 1
+
+    if n <= 0:
+        return pasTriList
+
+    for i in range(0, n):
+        lst = []
+        rc_even, rc_odd = 1, 2
+        if i == 0:
+            lst = [1]
+        elif i == 1:
+            lst = [1, 1]
+        else:
+            for j in range(0, m):
+                if j == 0 or j == (m - 1):
+                    lst.append(1)
+                else:
+                    lst.append(pasTriList[i-1][j] + pasTriList[i-1][j-1])
+
+        pasTriList.append(lst)
+        m += 1
+
+    return pasTriList
