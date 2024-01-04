16f32b8654821e05546db568cb91c1e1dafe1360
phila-hh <filimon.haftomh@gmail.com>
2024-01-04 09:42:29 -0800

    fix: syntax error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module contains the pascal_triangle function"""
 
+
 def pascal_triangle(n):
     """Returns a list of lists of integers representing the Pascal's
     triangle of n"""
