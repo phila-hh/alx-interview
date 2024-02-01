4273e667aff258b4025aa3d5ce8c2158ffd6508d
phila-hh <filimon.haftomh@gmail.com>
2024-02-01 04:36:35 -0800

    0x02. Minimum Operations project

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    0-minimum_operations module
+"""
+
+
+def minOperations(n):
+    """Calculates the fewest number of oprations needed"""
+    if type(n) not in int:
+        return 0
+    count = 0
+    clipboard = 0
+    done = 1
+    while n > done:
+        if clipboard == 0 or ((n - done) % done == 0 and n - done > 0):
+            clipboard = done
+            done += clipboard
+            count += 2
+        elif clipboard > 0:
+            done += clipboard
+            count += 1
+
+    return count
