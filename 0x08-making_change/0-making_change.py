7d2db8cb7dd72aff40c8c28e506ddb35dcc31534
phila-hh <filimon.haftomh@gmail.com>
2024-02-29 20:49:52 +0000

    fix: syntax error

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,16 +5,16 @@
 
 
 def makeChange(coins, total):
-	""" Determines the fewest number of coins needed to meet
-	a given amount total """
-	if total <= 0:
-		return 0
-	remainder = total
-	count = 0
-	index = 0
-	sorted_coins = sorted(coins, reverse=True)
-	length = len(coins)
-	while remainder > 0:
+    """ Determines the fewest number of coins needed to meet
+    a given amount total """
+    if total <= 0:
+        return 0
+    remainder = total
+    count = 0
+    index = 0
+    sorted_coins = sorted(coins, reverse=True)
+    length = len(coins)
+    while remainder > 0:
         if index >= length:
             return -1
         if 0 <= remainder - sorted_coins[index]:
